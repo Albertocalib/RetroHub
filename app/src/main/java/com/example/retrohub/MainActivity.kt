84912ed6ec99f7a40e8f0fe69d21a1e028ffa679
Ana <ani_al_or@hotmail.com>
2020-01-29 18:43:49 +0100

    - Add new font (catamaran) - Add layout for emptyview - Fix design in MainActivity

--- app/src/main/java/com/example/retrohub/MainActivity.kt
+++ app/src/main/java/com/example/retrohub/MainActivity.kt
@@ -5,6 +5,7 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,11 +16,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        button_new_retro.setOnClickListener { view ->
+            Toast.makeText(baseContext,R.string.commingSoon,Toast.LENGTH_SHORT).show()
         }
     }
+
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
