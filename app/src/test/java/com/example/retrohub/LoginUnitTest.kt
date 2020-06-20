f439702398733089d1e63554b492b5cd240699f5
Ana <ani_al_or@hotmail.com>
2020-06-20 11:36:24 +0200

    add extra login test
    onFailure, onSucces and onError, test viewmodel instead of API

--- app/src/test/java/com/example/retrohub/LoginUnitTest.kt
+++ app/src/test/java/com/example/retrohub/LoginUnitTest.kt
@@ -1,59 +1,87 @@
 package com.example.retrohub
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
 import androidx.lifecycle.observe
 import com.example.retrohub.model_view.LoginState
 import com.example.retrohub.model_view.LoginViewModel
 import com.example.retrohub.repository.UserRepository
+import com.example.retrohub.service.UserService
+import okhttp3.ResponseBody
+import okhttp3.internal.EMPTY_RESPONSE
 import org.junit.Test
 
 import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito.mock
 import org.mockito.junit.MockitoJUnitRunner
+import retrofit2.Call
+import retrofit2.Response
+import java.io.IOException
 
 
 private const val FAKE_STRING="1234"
 
 @RunWith(MockitoJUnitRunner::class)
-class LoginUnitTest {
+class LoginUnitTest : LifecycleOwner {
 
     @Rule
     @JvmField
     val rule = InstantTaskExecutorRule()
 
     @Mock private lateinit var userRepository: UserRepository
-    @Mock private lateinit var viewLifecycleOwner: LifecycleOwner
+    private val lifecycle = LifecycleRegistry(this)
+
+    override fun getLifecycle() = lifecycle
 
     @Test
-    fun login_with_valid_credentials(){
+    fun login_on_failure(){
         val vm = LoginViewModel(userRepository)
-        vm.getLogin(FAKE_STRING, FAKE_STRING)
-        vm.state.observe(viewLifecycleOwner, ::onChanged)
+        vm.state.observe(this, ::onError)
+        vm.onFailure(mock(Call::class.java) as Call<Boolean>,IOException())
     }
-    private fun onChanged(ignored:LoginState) { assert(true) }
+    private fun onError(state: LoginState?) { assertEquals(LoginState.ERROR,state) }
+
+
+    @Test
+    fun login_on_success(){
+        val vm = LoginViewModel(userRepository)
+        vm.state.observe(this, ::onSuccess)
+        vm.onResponse(mock(Call::class.java) as Call<Boolean>, Response.success(true))
+    }
+    private fun onSuccess(state: LoginState) { assertEquals(LoginState.SUCCESS,state) }
+
+
+    @Test
+    fun login_on_error(){
+        val vm = LoginViewModel(userRepository)
+        vm.state.observe(this, ::onError)
+        vm.onResponse(mock(Call::class.java) as Call<Boolean>, Response.error(405, EMPTY_RESPONSE))
+    }
 
     @Test
     fun login_with_empty_credentials() {
         val vm = LoginViewModel(userRepository)
         vm.getLogin("","")
-        assertEquals(LoginState.ERROR,vm.state.value)
+        onError(vm.state.value)
     }
 
     @Test
     fun login_without_username() {
         val vm = LoginViewModel(userRepository)
         vm.getLogin("", FAKE_STRING)
-        assertEquals(LoginState.ERROR, vm.state.value)
+        onError(vm.state.value)
     }
 
     @Test
     fun login_without_password() {
         val vm = LoginViewModel(userRepository)
-        vm.getLogin("1234", FAKE_STRING)
-        assertEquals(LoginState.ERROR, vm.state.value)
+        vm.getLogin(FAKE_STRING, "")
+        onError(vm.state.value)
     }
 
 
